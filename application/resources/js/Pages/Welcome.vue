be30b09edb178148e47d99018627e30e1bc990d5
Vinicius Viana <viniciuspro333@gmail.com>
2024-08-26 21:06:29 -0300

    feat: project create

--- application/resources/js/Pages/Welcome.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <Login />
-</template>
-
-<script setup>
-import Header from '@/Components/Header.vue';
-import Login from './Auth/Login.vue';
-</script>
---
+++ application/resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.header')
+
+@section('title' . 'Welcome')
+
+@section('content')
+
+<h1>Teste</h1>
+
+@endsection
