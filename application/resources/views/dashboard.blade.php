2ac4bdee4e20886e02e0b58c3b15db9d39124d50
Vinicius Viana <viniciuspro333@gmail.com>
2024-09-16 12:48:50 -0300

    feat: adding images

--- application/resources/views/dashboard.blade.php
+++ application/resources/views/dashboard.blade.php
@@ -8,13 +8,20 @@
 
     <body class="bg-slate-200">
         @if (count($pets) > 0)
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                @foreach ($pets as $pet)
-                    <p>{{ $pet->nome }}</p>
-                    <p>{{ $pet->pet }}</p>
-                @endforeach
-            </div>
-            </div>
+            <h1 class="text-3xl font-medium ml-4 text-orange-500">Meus Pets</h1>
+            @foreach ($pets as $pet)
+                <main class="flex flex-col p-2">
+                    <section class="grid grid-cols-1 gap-4 mt-2 sm:grid-cols-2 lg:grid-cols-3">
+                        <div
+                            class="bg-white p-3 rounded-lg min-h-[200px] flex flex-col relative transition-shadow duration-300 ease-in-out cursor-pointer hover:shadow-lg">
+                            <img class="rounded-lg w-full h-64 object-cover duration-300" src="/img/pets/{{ $pet->image }}"
+                                alt="{{ $pet->nome }}">
+                            <p class="text-orange-500 text-2xl">{{ $pet->nome }}</p>
+                            <p class="text-orange-500">{{ $pet->pet }}</p>
+                        </div>
+                    </section>
+                </main>
+            @endforeach
         @else
             <div class="flex justify-center items-center p-4">
                 <p>
--- application/resources/views/welcome.blade.php
+++ application/resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
 @section('title' . 'BEM VINDO')
 
 @section('content')
-    <link rel="icon" href="{{ asset('dog.png') }}" type="image/png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    <link rel="icon" href="{{ asset('dog.png') }}" type="image/png" />
     @vite('resources/css/app.css')
 
     <body class="bg-slate-200">
@@ -12,30 +13,44 @@
             <section class="grid grid-cols-1 gap-4 mt-5 sm:grid-cols-2 lg:grid-cols-3">
                 <div
                     class="bg-white p-3 rounded-lg min-h-[200px] flex flex-col relative transition-shadow duration-300 ease-in-out cursor-pointer hover:shadow-lg">
-
+                    <img class="rounded-lg w-full h-64 object-cover duration-300" src="/images/image1.jpg" alt="IA image">
+                    <p class="text-orange-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita aut
+                        provident, beatae quia
+                        perferendis.</p>
+                </div>
                 </div>
                 <div
                     class="bg-white p-3 rounded-lg min-h-[200px] flex flex-col relative transition-shadow duration-300 ease-in-out cursor-pointer hover:shadow-lg">
-
+                    <img class="rounded-lg w-full h-64 object-cover duration-300" src="/images/image2.jpg" alt="IA image">
+                    <p class="text-orange-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita aut
+                        provident, beatae quia
+                        perferendis.</p>
                 </div>
                 <div
                     class="bg-white p-3 rounded-lg min-h-[200px] flex flex-col relative transition-shadow duration-300 ease-in-out cursor-pointer hover:shadow-lg">
-
+                    <img class="rounded-lg w-full h-64 object-cover duration-300" src="/images/image3.jpg" alt="IA image">
+                    <p class="text-orange-500">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita aut
+                        provident, beatae quia
+                        perferendis.</p>
                 </div>
             </section>
             <section class="mt-6">
                 <h1 class="text-2xl font-medium text-orange-500">Veja os profissionais disponiveis para atender seu pet</h1>
-                <div class="mt-6">
-                    <p>Dr Manoel da Silva - Disponivel</p>
+                <div class="mt-6 p-2 rounded bg-white w-[300px]">
+                    <a class="font-semibold" href="#">Doutor Manél - Disponivel <i
+                            class="fa-sharp fa fa-arrow-right justify-end items-end ml-16"></i></a>
                 </div>
-                <div class="mt-6">
-                    <p>Dr Manoel da Silva - Disponivel</p>
+                <div class="mt-6 p-2 rounded bg-white w-[300px]">
+                    <a class="font-semibold" href="#">Doutor Manél - Disponivel <i
+                            class="fa-sharp fa fa-arrow-right justify-end items-end ml-16"></i></a>
                 </div>
-                <div class="mt-6">
-                    <p>Dr Manoel da Silva - Disponivel</p>
+                <div class="mt-6 p-2 rounded bg-white w-[300px]">
+                    <a class="font-semibold" href="#">Doutor Manél - Disponivel <i
+                            class="fa-sharp fa fa-arrow-right justify-end items-end ml-16"></i></a>
                 </div>
-                <div class="mt-6">
-                    <p>Dr Manoel da Silva - Disponivel</p>
+                <div class="mt-6 p-2 rounded bg-white w-[300px]">
+                    <a class="font-semibold" href="#">Doutor Manél - Disponivel <i
+                            class="fa-sharp fa fa-arrow-right justify-end items-end ml-16"></i></a>
                 </div>
             </section>
         </main>
