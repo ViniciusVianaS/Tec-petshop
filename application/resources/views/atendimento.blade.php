12c16fa28c6b7b4a21fbe24967f75fc00ef4d4ae
Vinicius Viana <viniciuspro333@gmail.com>
2025-05-05 10:25:15 -0300

    refactor: changing the .env and screen label

--- application/resources/views/atendimento.blade.php
+++ application/resources/views/atendimento.blade.php
@@ -10,7 +10,7 @@
             <form action="/atendimento" method="POST" class="mt-6 w-[300px]">
                 @csrf
                 <div class="mt-2">
-                    <label for="pet_id" class="block text-gray-700 font-bold mb-2">Selecione seu pet</label>
+                    <label for="pet_id" class="block text-gray-700 font-bold mb-2">Selecione o médico</label>
                     <select
                         class="block w-full text-gray-700 border-none rounded p-2 leading-tight focus:outline-none focus:bg-slate-100 focus:border-blue-500"
                         name="medico_id" id="medico" required>
@@ -20,7 +20,7 @@
                     </select>
                 </div>
                 <div class="mt-4">
-                    <label for="medico_id" class="block text-gray-700 font-bold mb-2">Selecione o médico</label>
+                    <label for="medico_id" class="block text-gray-700 font-bold mb-2">Selecione seu pet</label>
                     <select
                         class="block w-full text-gray-700 border-none rounded p-2 leading-tight focus:outline-none focus:bg-slate-100 focus:border-blue-500"
                         name="pet_id" id="pet" required>
